b0cc0a7e5b9814442c3def70acad9c5952e439a8
Astarosh <Mamo4ka5665928@mail.ru>
2017-10-25 09:26:39 +0200

    new commit

--- src/main/java/filter/HibernateSession.java
+++ src/main/java/filter/HibernateSession.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package filters;
+package filter;
 
 import java.io.IOException;
 import java.io.PrintStream;
